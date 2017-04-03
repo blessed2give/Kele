91dec9c9ba82acb9eb0ae7aec623e7f4aa31d74d
Paul <paucho1437@yahoo.com>
2017-04-03 14:12:59 -0500

    authoriztion and initalization COMPLETE

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,10 +2,17 @@ require 'httparty'
 
 class Kele
   include HTTParty
-  base_uri 'https://www.bloc.io/api/v1'
+  BASE_URI = 'https://www.bloc.io/api/v1'
 
-#  def initialize(e, p)
-#    @auth = { email: e, password: p }
-#  end
+  def initialize(email, password)
+    @email = email
+    @password = password
+  end
+
+  def get_me
+    options = { body: { email: @email, password: @password } }
+    response = self.class.post(BASE_URI + '/sessions', options)
+    @auth_token = response['auth_token']
+  end
 
 end
